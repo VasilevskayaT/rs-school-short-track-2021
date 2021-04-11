16f75bd86a931b00ea83c0239a8ca708cc1b9b71
VasilevskayaT <unost.work@gmail.com>
2021-04-11 23:54:59 +0300

    feat: edit solution of task 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,38 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matr) {
+  const arr = [...matr];
+  for (let k = 0; k < arr.length; k++) {
+    for (let l = 0; l < arr[k].length; l++) {
+      if (arr[k][l] === false) arr[k][l] = 0;
+      else arr[k][l] = 'bomb';
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === 'bomb') {
+        if (i - 1 >= 0) {
+          if (typeof arr[i - 1][j] === 'number') arr[i - 1][j]++;
+          if (j - 1 >= 0 && typeof arr[i - 1][j - 1] === 'number') arr[i - 1][j - 1]++;
+          if (j + 1 < arr[i].length && typeof arr[i - 1][j + 1] === 'number') arr[i - 1][j + 1]++;
+        }
+        if (i + 1 < arr.length) {
+          if (typeof arr[i + 1][j] === 'number') arr[i + 1][j]++;
+          if (j - 1 >= 0 && typeof arr[i + 1][j - 1] === 'number') arr[i + 1][j - 1]++;
+          if (j + 1 < arr[i].length && typeof arr[i + 1][j + 1] === 'number') arr[i + 1][j + 1]++;
+        }
+        if (typeof arr[i][j - 1] === 'number') arr[i][j - 1]++;
+        if (typeof arr[i][j + 1] === 'number') arr[i][j + 1]++;
+      }
+    }
+  }
+  for (let n = 0; n < arr.length; n++) {
+    for (let m = 0; m < arr[n].length; m++) {
+      if (arr[n][m] === 'bomb') arr[n][m] = 1;
+    }
+  }
+  return arr;
 }
 
 module.exports = minesweeper;
