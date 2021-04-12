788f0d4ff84db48981284439d96e12db466790a7
VasilevskayaT <unost.work@gmail.com>
2021-04-13 01:25:13 +0300

    feat: implement task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(strArg) {
+  return strArg.split('').slice(1).reduce((acc, val, index, arr) => {
+    if (acc.currentVal === val) {
+      acc.counts += 1;
+
+      if (index === arr.length - 1) {
+        acc.resultString += `${acc.counts === 1 ? '' : acc.counts}${acc.currentVal}`;
+      }
+
+      return acc;
+    }
+    if (index === arr.length - 1) {
+      acc.resultString += `${acc.counts === 1 ? '' : acc.counts}${acc.currentVal}${val}`;
+      return acc;
+    }
+    acc.resultString += `${acc.counts === 1 ? '' : acc.counts}${acc.currentVal}`;
+    acc.currentVal = val;
+    acc.counts = 1;
+    return acc;
+  }, { currentVal: strArg[0], counts: 1, resultString: '' }).resultString;
 }
 
 module.exports = encodeLine;
