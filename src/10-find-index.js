340feb58227c6589025f0880993b092f1d5c88a5
VasilevskayaT <76494423+VasilevskayaT@users.noreply.github.com>
2021-04-12 03:32:03 +0300

    feat: implement task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,8 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(arr, val) {
+  return arr.indexOf(val);
 }
 
 module.exports = findIndex;
